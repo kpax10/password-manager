9bc3fc69dd00cfa87dedde83b673a5a582dffccc
Kyle <notxap55@yahoo.com>
2024-09-04 16:24:06 -0700

    working on password

--- main.py
+++ main.py
@@ -1,7 +1,49 @@
 from tkinter import *
+from tkinter import messagebox
+import random
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
+#Password Generator Project
+import random
+letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
 
+password_letters = [random.choice(letters) for char in range(random.randint(8, 10))]
+password_symbols = [random.choice(symbols) for symbol in range(random.randint(2, 4))]
+password_numbers = [random.choice(numbers) for num in range(random.randint(2, 4))]
+
+combined_password = password_numbers + password_symbols + password_letters
+random.shuffle(combined_password)
+print(''.join(combined_password))
+
+
+
+# random.shuffle(password_list)
+#
+# password = ""
+# for char in password_list:
+#   password += char
+#
+# print(f"Your password is: {password}")
 # ---------------------------- SAVE PASSWORD ------------------------------- #
+def save():
+    website = website_entry.get()
+    username = username_entry.get()
+    password = password_entry.get()
+
+    if website == '' or username == '' or password == '':
+        messagebox.showwarning('Warning', "Please don't leave any fields empty!")
+        return
+
+    response = messagebox.askokcancel(title=website, message="Save site details?")
+
+    if response:
+        with open('data.txt', 'a') as file:
+            file.write(f"{website} | {username} | {password}\n")
+
+        website_entry.delete(0, END)
+        password_entry.delete(0, END)
+        website_entry.focus_set()
 
 # ---------------------------- UI SETUP ------------------------------- #
 window = Tk()
@@ -32,7 +74,7 @@ password_entry.grid(column=1, row=3, sticky=EW)
 password_button = Button(text='Generate Password', pady=0, bg='white', highlightthickness=0)
 password_button.grid(column=2, row=3, sticky=EW)
 
-add_button = Button(text='Add', bg='white', highlightthickness=0, pady=0)
+add_button = Button(text='Add', bg='white', highlightthickness=0, pady=0, command=save)
 add_button.grid(column=1, columnspan=2, row=4, sticky=EW)
 
 
